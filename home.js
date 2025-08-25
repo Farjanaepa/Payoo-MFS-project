6b616bfe6771af2a7c8f519f5f36d2596772fec9
Farjanaepa <farjanaepa@gmail.com>
2025-08-26 00:50:35 +0600

    add button form hide show

--- home.js
+++ home.js
@@ -31,3 +31,17 @@ document.getElementById("add-money-btn").addEventListener("click",function(e){
     document.getElementById("available-balance").innerText = 
     totalNewBalance
 })
+
+
+// toggling feature
+
+document.getElementById("add-button").addEventListener("click",function(){
+    document.getElementById("cash-out-parent").style.display = "none"
+    document.getElementById("add-money-parent").style.display = "block"
+})
+
+document.getElementById("cash-out-button").addEventListener("click",function(){
+       document.getElementById("add-money-parent").style.display = "none"
+    document.getElementById("cash-out-parent").style.display = "block"
+ 
+})
